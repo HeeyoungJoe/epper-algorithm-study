a2b24126dd5603b3ad42936018a32dbb1b824518
Heeyoung <HeeyoungJoe2000@gmail.com>
2021-03-23 11:27:24 +0900

    Epper이후

--- EpperPrep/page9_number8.c
+++ EpperPrep/page9_number8.c
@@ -21,9 +21,51 @@ int is_there_zero(int A[][20], int M, int N) {
 	return 0;
 }
 
-int rot(int** A, int row, int col, int M, int N) {
-	int day[4] = { 0 };
+typedef struct Node {
+	struct Node* next;
+	int x;
+	int y;
+}Node;
+typedef struct{
+	Node* head;
+}LinkedList;
+//LinkedList format so that 1s can be kept
+LinkedList* makeList() {
+	LinkedList* L = (LinkedList*)malloc(sizeof(LinkedList));
+	L->head = NULL;
+	return L;
+}
+Node* makeNode(int x, int y) {
+	Node* node = (Node*)malloc(sizeof(Node));
+	node->x = x;
+	node->y = y;
+	node->next = NULL;
+	return node;
+}
+void addFirst(int x, int y, LinkedList* L) {
+	Node* node = makeNode(x, y);
+	Node* temp = L->head;//있으면 다음노드, 없으면 NULL;
+	node->next = temp;
+	L->head = node;
+}
+int rot(int** A, LinkedList* ones, int M, int N) {
+	Node* temp = ones->head;
+	while (1) {
+		while (temp->next != NULL) {
+			temp = temp->next;
+		}
+	}
+	//while(1)
+	//for all of (row,col)--> this is one day
+	// 	   make the adjacents 1 
+	// 	   keep yes_change var that is initialied to 1 if any of the ifs are used
+	// 	   add the became-one adjacent coordinates to row, col --how? 
+	//			if row col are linked list, addFirst will do the trick
+	// if yes_change==1, continue the for loop and increase day
+	// 	   if yes_change ==0, break 
+	// 	   at the end of the loop yes_change is initialized to zero
 	//top
+	int day[4] = { 0 };
 	if (row + 1 < M) {
 		if (A[row + 1][col] == 0) {
 			*(A + (row + 1) * N + col) = 1;
@@ -79,13 +121,12 @@ int will_it_rot(int A[][20],int M, int N,int i,int j) {
 	}
 }
 void main() {
-	int M = 1;
+	int box[20][20] = { -1 };
+	/*int M = 1;
 	int N = 1;
 	scanf_s("%d", &M);
 	scanf_s("%d", &N);
 	//1이 익은거, 0이 안 익은거, -1이 없는거
-	
-	int box[20][20] = { -1 };
 	
 	//initialize first state
 	for (int i = 0; i < M; i++) {
@@ -99,10 +140,25 @@ void main() {
 	int where_1_r= rand() % M;
 	int where_1_c = rand() % N;
 	box[where_1_r][where_1_c] = 1;
-
 	//show array
 	printarray(box, M, N);
 
 	//calculate 
 	printf("%d", will_it_rot(box, M, N, where_1_r, where_1_c));
+	*/
+
+
+	//test set 1
+	// 	   0	0	0	1
+	// 	   0	0	0	0
+	// 	   0	0	0	0
+	// 	   0	0	0	0
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			box[i][j] = 0;
+		}
+	}
+	box[0][3] = 1;
+	printarray(box, 4, 4);
+	will_it_rot(box,4,4,)
 }
